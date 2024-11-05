d685f6ff8c776a00e8d2837940fe68b8b69e267b
roufri <rouven@roufri.com>
2024-11-05 11:35:15 +0100

    add a button to generate and display an aggregated list of all ingredients for the selected recipes

--- cookingplanner-ui/src/components/RecipeList.js
+++ cookingplanner-ui/src/components/RecipeList.js
@@ -5,6 +5,7 @@ import Recipe from "./Recipe";
 export default function RecipeList() {
     const [recipes, setRecipes] = useState([]);
     const [randomRecipes, setRandomRecipes] = useState([]);
+    const [aggregatedIngredients, setAggregatedIngredients] = useState([]);
 
     useEffect(() => {
         const fetchAllRecipes = async () => {
@@ -31,19 +32,44 @@ export default function RecipeList() {
             });
         }
     };
-    
+
+    const generateIngredientList = async () => {
+        // Extracting the IDs of selected recipes
+        const recipeIds = randomRecipes.map(recipe => recipe.id);
+        try {
+            const response = await axios.get(`http://localhost:8080/api/recipes/ingredients?recipeIds=${recipeIds}`);
+            setAggregatedIngredients(response.data);
+            console.log(aggregatedIngredients);
+        } catch (error) {
+            console.error("Error fetching aggregated ingredients:", error);
+        }
+    };
+
     return (
-        <div>
-            <h1>Random Recipes</h1>
-            <ul>
-                {randomRecipes.map(recipe => (
-                    <Recipe 
-                        key={recipe.id} 
-                        recipe={recipe}
-                        reroll={() => reroll(recipe.id)}
-                    />
-                ))}
-            </ul>
-        </div>
+        <>
+            <div>
+                <h1>Random Recipes</h1>
+                <ul>
+                    {randomRecipes.map(recipe => (
+                        <Recipe 
+                            key={recipe.id} 
+                            recipe={recipe}
+                            reroll={() => reroll(recipe.id)}
+                        />
+                    ))}
+                </ul>
+            </div>
+            <div class="aggregated-ingredients">
+                <button onClick={generateIngredientList}>Generate Ingredient List</button>
+                <h3>Sum of Ingredients</h3>
+                <ul>
+                    {aggregatedIngredients.map((ingredient, index) => (
+                        <li key={index}>
+                            {ingredient.ingredient}: {ingredient.quantity} {ingredient.unit}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
     );
 };
